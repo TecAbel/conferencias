0be374d264b0475a964c31320191a943a60bf961
TecAbel <abelardo96.work@gmail.com>
2019-11-07 21:03:32 -0600

    retroceso fecha

--- js/main.js
+++ js/main.js
@@ -145,4 +145,20 @@ $(function(){
         $(enlace).fadeIn(1000);
         return false;
     });
+
+    //animaciones numeros 
+
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6},1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15},1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3},1200);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9},1200);
+
+    //cuenta regresiva 
+
+    $('.cuenta-regresiva').countdown('2019/11/20 09:00:00', function (e){
+        $('#dias').html(e.strftime('%D'));
+        $('#horas').html(e.strftime('%H'));
+        $('#minutos').html(e.strftime('%M'));
+        $('#segundos').html(e.strftime('%S'));
+    })
 });
